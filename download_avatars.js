a2eba6f6bb0d5d192a43092b8929decf2c3fd9e2
Amy <amy@lighthouselabs.ca>
2017-07-31 17:17:24 +0000

    Printing body of HTTP request

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,23 @@
 var request = require('request');
+var GITHUB_USER = "notadeejay";
+var GITHUB_TOKEN = "8475571afba7e2af0eb95296f64a11e3b4e09ba3";
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+
+
+  request.get(requestURL)
+  .on('data', function (chunk) {
+      console.log('BODY :' + chunk);
+  })
+
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
