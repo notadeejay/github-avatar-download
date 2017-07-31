e9068e8eec44533965edb94d6bed107bef95709d
Amy <amy@lighthouselabs.ca>
2017-07-31 18:15:51 +0000

    Find and print the URL of Github contributors

--- download_avatars.js
+++ download_avatars.js
@@ -5,19 +5,29 @@ var GITHUB_TOKEN = "8475571afba7e2af0eb95296f64a11e3b4e09ba3";
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
+  var options = {
+    "url": requestURL,
+    "method": "GET",
+    "headers": {
+      'User-Agent': 'GitHub Avatar Download - Student Project'
+    }
+  };
 
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  request(options, function (err, res, body) {
+    var contibutors = JSON.parse(body);
+    cb(err, contibutors);
+  });
+};
 
 
-  request.get(requestURL)
-  .on('data', function (chunk) {
-      console.log('BODY :' + chunk);
-  })
-
-
+function loopResults(err, result) {
+  console.log("Errors:", err);
+  for (let contributor of result) {
+    console.log('https://avatars0.githubusercontent.com/u/' + contributor.id + '?v=4')
+  }
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+
+
+getRepoContributors("jquery", "jquery", loopResults);
