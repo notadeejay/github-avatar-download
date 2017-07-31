37e3336f0ef125d3c0e698c36f00858ed48a90aa
Amy <amy@lighthouselabs.ca>
2017-07-31 20:07:16 +0000

    Downloading Github contributor avatars with HTTP requests

--- download_avatars.js
+++ download_avatars.js
@@ -2,11 +2,11 @@ var request = require('request');
 var GITHUB_USER = "notadeejay";
 var GITHUB_TOKEN = "8475571afba7e2af0eb95296f64a11e3b4e09ba3";
 var fs = require('fs');
-var input = process.argv.slice(2)
+
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors'
+  var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
   var options = {
     "url": requestURL,
@@ -17,23 +17,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   if (typeof(repoOwner) == 'undefined' || typeof(repoName) == 'undefined') {
-    throw new Error ('Incorrect input');
+    throw new Error('Incorrect input');
   }
 
-  request(options, function (err, res, body) {
+  request(options, function(err, res, body) {
     var contibutors = JSON.parse(body);
     cb(err, contibutors);
   });
-};
+}
+
 
 function downloadImageByURL(URL, filePath) {
   request.get(URL)
-    .on('error', function (err) {
+    .on('error', function(err) {
       throw err;
     })
 
     .on('end', function() {
-      console.log('Download complete')
+      console.log('Download complete');
     })
 
     .pipe(fs.createWriteStream(filePath))
@@ -43,10 +44,9 @@ function downloadImageByURL(URL, filePath) {
 
 function loopResults(err, result) {
   for (var contributor of result) {
-    downloadImageByURL(contributor.avatar_url,'./avatars/' + contributor.login + '.jpg');
+    downloadImageByURL(contributor.avatar_url, './avatars/' + contributor.login + '.jpg');
   }
 }
-
 
 
 getRepoContributors(process.argv[2], process.argv[3], loopResults);
